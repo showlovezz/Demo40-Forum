ab03266140016e385ffb645ffca851b88fbc9223
vita <dragic.hsieh@gmail.com>
2018-06-21 17:14:48 +0800

    Done add photo upload using Carrierwave gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 	belongs_to :user
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :content, presence: true
+	validates :image, presence: true
+	mount_uploader :image, PhotoUploader
 end
