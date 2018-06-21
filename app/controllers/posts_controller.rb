c95ffc35a044c69e2dbc135240a2b28f47cbd718
vita <dragic.hsieh@gmail.com>
2018-06-21 15:18:09 +0800

    Done add basic style and navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@posts = Post.order("created_at DESC")
