b246508eeb4b4dd4a9b5a744951f90ea71f45d6a
vita <dragic.hsieh@gmail.com>
2018-06-21 12:25:01 +0800

    Done add time in index page and change post order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@posts = Post.all
+		@posts = Post.order("created_at DESC")
 	end
 
 	def new
