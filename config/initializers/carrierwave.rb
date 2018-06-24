c16d6c8b52ea62ba94f15354e31b65bc0f6341a8
vita <dragic.hsieh@gmail.com>
2018-06-24 15:05:57 +0800

    Modify s3 key i add ENV

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
   if Rails.env.production?
 	  config.fog_credentials = {
 	    provider:              'AWS',                        # required
-	    aws_access_key_id:     'AWS_ACCESS_KEY_ID',          # required
-	    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',      # required
-	    region:                'ap-northeast-1'              # optional, defaults to 'us-east-1'
+	    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],          # required
+	    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # required
+	    region:                ENV["ap-northeast-1"]              # optional, defaults to 'us-east-1'
 	  }
-	  config.fog_directory  = 'S3_BUCKET_NAME'               # required
+	  config.fog_directory  = ENV["S3_BUCKET_NAME"]               # required
 	else
 		config.storage :file
 	end
