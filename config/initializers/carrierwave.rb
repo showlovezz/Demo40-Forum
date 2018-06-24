5b92e41fe032f6596769838ccc7a752c28380de5
vita <dragic.hsieh@gmail.com>
2018-06-24 15:26:37 +0800

    Debug ENV[AWS_REGION] wrong message

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
 	    provider:              'AWS',                        # required
 	    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],          # required
 	    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # required
-	    region:                ENV["ap-northeast-1"]              # optional, defaults to 'us-east-1'
+	    region:                ENV["AWS_REGION"]              # optional, defaults to 'us-east-1'
 	  }
 	  config.fog_directory  = ENV["S3_BUCKET_NAME"]               # required
 	else
